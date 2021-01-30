2a74042e62c88fa7fd08902e7813be32dbe30c74
SamMohamedGoher <madrasy101@gmail.com>
2021-01-30 05:03:31 +0200

    Add subscription model
    - Create schema of subscription
    - Add messages of validation errors
    - Add relations between subscription model with all of guardian, child, mentor and course models

---
+++ models/subscriptions.js
@@ -0,0 +1,84 @@
+// Load required packages
+const mongoose = require(`mongoose`);
+
+// create subscription schema
+const subscriptionSchema = new mongoose.Schema({
+  guardianId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: `guardians`,
+    required: [true, `please add guardian id.`]
+  },
+  childId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: `children`,
+    required: [true, `please add child id.`]
+  },
+  mentorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: `mentors`,
+    required: [true, `please add mentor id.`]
+  },
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: `courses`,
+    required: [true, `please add course id.`]
+  },
+  creatingDate: {
+    type: Date,
+    default: Date.now()
+  },
+  balance: {
+    type: Number,
+    required: [true, `please add cost of course.`]
+  },
+  complete: {
+    type: Boolean,
+    default: false
+  },
+  appointments: {
+    type: [{
+      _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: mongoose.Types.ObjectId()
+      },
+      date: {
+        type: Date,
+        default: Date.now()
+      },
+      title: String,
+      mentor: {
+        type: Boolean,
+        default: false
+      },
+      child: {
+        type: Boolean,
+        default: false
+      }
+    }]
+  },
+  notes: {
+    type: [{
+      appointmentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, `please add appoinement id.`]
+      },
+      date: Date,
+      description: {
+        type: String,
+        required: [true, `please add description to note`]
+      },
+    }]
+  },
+  liveUrl: {
+    type: String,
+    match: [/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      `invalid url.`
+    ]
+  },
+  liveUrlExpire: Date
+});
+
+// connect schema with subscription collection by creating subscription model
+const Subscription = mongoose.model(`subscriptions`, subscriptionSchema);
+
+module.exports = Subscription;
