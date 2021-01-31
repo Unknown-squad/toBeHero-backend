9d7c772f0c4887364576e9c73d24a6accbd9d23f
Mahmoud Serag <sragmahmoud@gmail.com>
2021-01-31 20:03:54 +0200

    Edit guardians model - Add validation message to fullName property - Remove min and max length from password property - Add url match to picture property - Add match to phone number property
    fix #7

--- models/guardians.js
+++ models/guardians.js
@@ -6,24 +6,24 @@ const GuardianSchema = new mongoose.Schema({
   fullName: {
     type: String,
     required: [true, 'Please add a name'],
-    minlength: 5,
-    maxlength: 50
+    minlength: [5, 'Min length is 5 character'],
+    maxlength: [50, 'Max length is 50 character']
   },
   email: {
     type: String,
     required: [true, 'Please add an emial'],
-    unique: [true, 'The email address is already taken, Please choose another'],
+    unique: true,
     match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid email']
   },
   password: {
     type: String,
-    required: [true, 'Please add a password'],
-    minlength: 8,
-    maxlength: 64
+    required: [true, 'Please add a password']
   },
   picture: {
     type: String,
-    required: false
+    match: [/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      `invalid url.`
+    ]
   },
   phone: {
     type: String,
@@ -43,45 +43,41 @@ const GuardianSchema = new mongoose.Schema({
     {
       cardNumber: {
         type: String,
-        minlength: 16,
-        maxlength: 16,
+        match: [/^\d{16}$/, 'Invalid card number'],
         required: [true, 'Please add a card number']
       },
-      expire: {
-        type: String,
-        minlength: 5,
-        maxlength: 5,
+      expireDate: {
+        type: Date,
         required: [true, 'Please add a expire']
       },
       CVV: {
         type: String,
-        minlength: 3,
-        maxlength: 3,
+        match: [/^\d{16}$/, 'Invalid CVV number'],
         required: [true, 'Please add a CVV']
       },
       nameOnCard: {
         type: String,
-        minlength: 10,
-        maxlength: 50,
+        minlength: [10, 'Min length is 10 character'],
+        maxlength: [50, 'Max length is 50 character'],
         required: [true, 'Please add a name on card']
       },
       phoneNumber: {
-        type: String,
-        required: [true, 'Please add a phone number']
+      type: String,
+      required: [true, 'Please add a phone number'],
+      unique: [true, 'The phone number is already taken, Please choose another'],
+      match: [/^\d{10}$/, 'Phone number maxlength is 10 characters']
       },
       _id: false
     }
   ],
   varificationToken: String,
   varificationTokenExpire: {
-    type: Date,
-    default: Date.now
+    type: Date
   },
   childrenId: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'children',
-      required: true
+      ref: 'children'
     }
   ]
 });
