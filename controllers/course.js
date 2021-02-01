da653680d9be833a7140f4c75590be868294e7fb
SamMohamedGoher <madrasy101@gmail.com>
2021-02-02 01:07:29 +0200

    Finish api "get courses"
    - Filter courses data
    - Get data by genre
    - Sort data by particualr topic
    - Set pagination
    - Filter data by rating

--- controllers/course.js
+++ controllers/course.js
@@ -25,3 +25,72 @@ exports.getOneCourse = asyncHandler(async (req, res) => {
   });
 
 });
+
+// @route   GET `/api/v1/courses`
+// @desc    get courses
+// @access  public
+exports.getCourses = asyncHandler(async (req, res) => {
+
+  // Genre
+  if(!req.query.genre) {
+    throw new Error(`no genre`);
+  }
+
+  // Sort
+  let sortby;
+  if(req.query.sortby === `rating`) {
+    sortby = `-rate`;
+  } else if(req.query.sortby === `popularity`) {
+    sortby = `-reviewCounter`;
+  } else if(req.query.sortby === `newest`) {
+    sortby = `-creatingDate`;
+  } else if(req.query.sortby === `high-price`) {
+    sortby = `-price`;
+  } else if(req.query.sortby === `low-price`) {
+    sortby = `price`;
+  } else {
+    sortby = `-reviewCounter`;
+  }
+
+  // filter by rate
+  let filter = 0;
+  if(req.query.ratings) {
+    filter = req.query.ratings;
+  }
+
+  // page
+  let page = parseInt(req.query.page, 10) || 1;
+  let limit = 10;
+  if(page < 0) {
+    page = 1;
+  }
+  
+  // find and filter courses
+  const courses = await Course
+  .find({genre: req.query.genre, rate: {$gte: filter}})
+  .sort(sortby)
+  .skip((page - 1) * limit)
+  .limit(limit)
+  .select(` -creatingDate -topicsList -mediaURLS -subscriptionNumber -reviewsId`)
+  .populate({
+    path: "mentorId",
+    select: "_id fullName picture isAvailable",
+    model: Mentor
+  });
+
+  // check if there is no courses in page
+  if(courses.length == 0) {
+    throw new Error(`no data in page`);
+  }
+  
+  res.status(200).json({
+    success: true,
+    message: `successfully get courses data of page ${page}`,
+    data: {
+      kind: `courses`,
+      count: courses.length,
+      items: courses
+    }
+  });
+
+});
--- routes/course.js
+++ routes/course.js
@@ -1,11 +1,17 @@
 // load required packages
 const express = require(`express`);
 const router = express.Router();
-const {getOneCourse} = require(`../controllers/course`);
+const {getOneCourse,
+       getCourses} = require(`../controllers/course`);
 
 // @route   GET `/api/v1/courses/:id`
 // @desc    get one course
 // @access  public
 router.get(`/api/v1/courses/:id`, getOneCourse);
+
+// @route   GET `/api/v1/courses`
+// @desc    get courses
+// @access  public
+router.get(`/api/v1/courses`, getCourses);
 
 module.exports = router;
