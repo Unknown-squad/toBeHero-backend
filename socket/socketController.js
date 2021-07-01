5dacc527e975ecb4a2fd2bf3ae294219c4e9806d
SamGoher <sam.m.goher@gmail.com>
2021-07-01 17:31:20 +0200

    Add socket.io functionality
    - Allow user to join room
    - Add activate live button functionality
    - Add deactivate live button functionality

--- socket/socketController.js
+++ socket/socketController.js
@@ -10,7 +10,36 @@ exports.ioServer = (httpServer) => {
   // setting controller
   io.on(`connect`, (socket) => {
 
-    
+    // join to room
+    socket.on(`join-room`, (subscriptionId, appointmentId) => {
+
+      // create room id
+      const roomId = `${subscriptionId}A${appointmentId}`;
+
+      // join user to room
+      socket.join(roomId);
+
+      // listening to start-live event
+      io.on(`start-live`, () => {
+
+        // create event to active live button
+        io.to(roomId).emit(`activate-button`, roomId);
+
+        // save appointment activation in database
+
+      });
+
+      // listening to end-live event
+      io.on(`end-live`, () => {
+
+        // create event to active live button
+        io.to(roomId).emit(`deactivate-button`, true);
+
+        // save appointment deactivation in database
+        
+      });
+
+    });
     
   });
 
