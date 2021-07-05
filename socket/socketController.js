901a58e911b5d22ecd5852ef0cc82b205d5c61f5
SamGoher <sam.m.goher@gmail.com>
2021-07-05 21:50:58 +0200

    Add appointment de/activating to database

--- socket/socketController.js
+++ socket/socketController.js
@@ -14,33 +14,57 @@ exports.ioServer = (httpServer) => {
 
   // setting controller
   io.on(`connect`, (socket) => {
-    // await activeAppointment(subscriptionId, appointmentId, true);
-
+    
+    // send id of child to mentor
     socket.emit("me", socket.id);
+    
+    // listen to event that come from mentor to active button that allow child to open live page
+    socket.on("mentor-in", async (subscriptionId, appointmentId) => {
 
-    socket.on("mentor-in", (subscriptionId) => {
+      // active 'join live' button to child
       io.emit(`child-in${subscriptionId}`, true);
-    });
 
+      // save appointment activation to database
+      await activeAppointment(subscriptionId, appointmentId, true);
+
+    });
+    
+    // listening to event that allow child to send his socket id to mentor
     socket.on(`hero-send-id`, (subscriptionId) => {
-      console.log("hero id is" + socket.id);
+      
+      // send socket id of child to mentor
       io.emit(`subscription${subscriptionId}`, socket.id);
-    });
 
-    socket.on("disconnect", () => {
+    });
+    
+    // listening to event that raised if call is ended
+    socket.on("disconnect", async (subscriptionId, appointmentId) => {
+
+      // raise event that notify user that call ended
       socket.broadcast.emit("callEnded");
+
+      // save appointment deactivation to database
+      await activeAppointment(subscriptionId, appointmentId, false);
     });
 
+    // listening to event that recieves video data
     socket.on("callUser", (data) => {
+
+      // forward video data to another user
       io.to(data.userToCall).emit("callUser", {
         signal: data.signalData,
         from: data.from,
         name: data.name,
       });
+
     });
 
+    // listening to event that comes from mentor to allow child to answer the call
     socket.on("answerCall", (data) => {
+
+      // raise event to allow child to answer the call
       io.to(data.to).emit("callAccepted", data.signal);
+      
     });
   });
 };
